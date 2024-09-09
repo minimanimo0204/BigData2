03244952d7bdd969473d2c21ddcad356b11e410d
minimanimo <minimanimo@minimanimoui-MacBookPro.local>
2024-09-09 12:38:17 +0900

    v.0.6

--- week03/week03_pandas.py
+++ week03/week03_pandas.py
@@ -1,11 +1,38 @@
 import pandas as pd
 
-df = pd.DataFrame(
-    [
+data = [
         [14, 72, 100],
         [51, 8, 72],
         [6, 72, 12]
-    ],
-    index=[1, 2, 3], columns=["a", "b", "c"]
-)
+    ]
+
+df = pd.DataFrame(data,  index=[1, 2, 3], columns=["a", "b", "c"])
 print(df)
+print(df.shape, df.dtypes)
+print(df['b'].value_counts())
+print(df['b'].nunique())
+print(df.describe())
+
+# import pandas as pd
+#
+# df = pd.DataFrame(
+#     {
+#         "a": [14, 51, 6],
+#         "b": [72, 8, 9],
+#         "c": [100, 11, 12]
+#     }, index=[1, 2, 3]
+# )
+# print(df)
+# df2 = pd.melt(df)\
+#     .rename(columns={'variable':'var', 'value':'val'})\
+#     .query("val > 50")
+# print(df2.tail(2))
+
+# import numpy as np
+#
+# m = np.array([[1,1,2,2],
+#               [3,3,4,4],
+#               [5,6,7,8],
+#               [9,9,9,0]])
+# print(m[::2, 1::2])
+# print(m[::2, 1])
